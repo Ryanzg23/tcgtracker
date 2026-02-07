6f4dc26f8faecf9f35747a308e565594da5a425d
Ryanzg23 <ryanzgregorio@gmail.com>
2026-02-08 00:41:06 +0800

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,1 +1,14 @@
 
+let CARD_DB = null;
+
+export async function loadCards() {
+  if (CARD_DB) return CARD_DB;
+
+  const res = await fetch("/data/cards.json");
+  CARD_DB = await res.json();
+  return CARD_DB;
+}
+
+export function getCard(cardId) {
+  return CARD_DB?.[cardId] || null;
+}
